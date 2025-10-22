e014e56296bc6fadae91f6bab54a4534f8244b07
tontomas <togarcial@unal.edu.co>
2025-10-21 23:42:56 -0500

    Se crea el primer hola mundo en Rust

---
+++ hola_mundo/target/debug/deps/hola_mundo.d
@@ -0,0 +1,5 @@
+C:\Users\Samot\Documents\Repos\Rust-learning\hola_mundo\target\debug\deps\hola_mundo.d: src\main.rs
+
+C:\Users\Samot\Documents\Repos\Rust-learning\hola_mundo\target\debug\deps\hola_mundo.exe: src\main.rs
+
+src\main.rs:
---
+++ hola_mundo/target/debug/hola_mundo.d
@@ -0,0 +1,1 @@
+C:\Users\Samot\Documents\Repos\Rust-learning\hola_mundo\target\debug\hola_mundo.exe: C:\Users\Samot\Documents\Repos\Rust-learning\hola_mundo\src\main.rs
