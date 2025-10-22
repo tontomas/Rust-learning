c4fabcca18f99db2170c288c562f425a69ae61e6
tontomas <togarcial@unal.edu.co>
2025-10-21 23:59:48 -0500

    entendimiento de Rust - Ejercicio 00: Hola, mundo!

---
+++ basics_00/target/debug/deps/basics_00-c28d6ed2e8f30d8b.d
@@ -0,0 +1,5 @@
+C:\Users\Samot\Documents\Repos\Rust-learning\basics_00\target\debug\deps\basics_00-c28d6ed2e8f30d8b.d: src\main.rs
+
+C:\Users\Samot\Documents\Repos\Rust-learning\basics_00\target\debug\deps\libbasics_00-c28d6ed2e8f30d8b.rmeta: src\main.rs
+
+src\main.rs:
